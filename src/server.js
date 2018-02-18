01bee5d74f7aa3dcc70342b4bd33c6653a621546
Павел <bavenka@gmail.com>
2018-02-18 20:44:43 +0300

    connected to MongoDB with mongoose applying

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 import express from'express';
 import graphqlHTTP from 'express-graphql';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 import schema from './shema';
+import config from '../config';
 
+const port = 4000;
+const { MONGO_URI } = config;
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
@@ -10,4 +15,14 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
-app.listen(4000);
+mongoose.connect(MONGO_URI);
+mongoose.Promise = global.Promise;
+mongoose.connection
+  .on('error', console.error.bind(console, 'MongoDB connection error:'))
+  .once('open', () => {
+    app.listen(port);
+    console.log(`We are live on ${port}`)
+  });
+
+
+
